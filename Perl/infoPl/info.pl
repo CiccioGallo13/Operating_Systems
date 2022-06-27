b7514ca5e3edc1678dbb9e1c26257a3df665becc
Francesco Gallo <francescogallo728@gmail.com>
2022-06-28 00:05:13 +0200

    perl ex

--- Perl/infoPl/info.pl
+++ Perl/infoPl/info.pl
@@ -7,7 +7,44 @@ if($#ARGV==1)
     ($ARGV[1] =~ /-b/ or $ARGV[1] =~ /-t=.*/) or die "invalid arguments";
 }
 
+@stat = qx(stat $ARGV[0]);
+$stat[1] =~ /directory/ or die "not a directory";
 @files = qx(ls $ARGV[0]);
+%size_name = ();
+$filetag = ".*";
+if($ARGV[1] =~ /-t=(.*)/)
+{
+    $filetag = $1;
+}
 
+$sizeF = 0;
+%name_size=();
+while(<@files>)
+{   $filename = $_;
+    @stat = qx(stat $ARGV[0]/$filename);
+    if(not($stat[1] =~ /directory/) and $stat[1] =~ /$filetag$/)
+    {   
+        $stat[1] =~ /Blocks: (\d+)\s+ IO Block: (\d+)/;
+        $size_name{$1*$2} = $filename;
+        $name_size{$filename} = $1*$2;
+        $sizeF += $1*$2;
+    }
+}
+if($#ARGV == 0 || $ARGV[1] =~ /-t/)
+{
+    use List::MoreUtils qw( minmax );
+    ($min, $max)= minmax (keys %size_name);
+    print "SizeFolder della cartella $ARGV[0] = $sizeF\n";
+    print "Max: $size_name{$max} ---> $max\n";
+    print "Min: $size_name{$min} ---> $min\n";
+}
+elsif($ARGV[1] =~ /-b/)
+{
+    open($fh, ">", "output.log");
+    foreach(sort {($name_size{$b} <=> $name_size{$a}) || $a cmp $b} keys %name_size )
+    {
+        print $fh "$_ --> $name_size{$_}\n";
+    }
+    close($fh);
+}
 
-print "@files";
